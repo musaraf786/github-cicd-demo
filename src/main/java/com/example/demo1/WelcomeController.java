c4f8bc79001ff560dcfd9994e66b150857238d15
hp <hp@DESKTOP-9MUA7A7>
2023-10-15 20:10:11 +0530

    code

---
+++ src/main/java/com/example/demo1/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.example.demo1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+	@GetMapping("/welcome")
+	public String Welcome() {
+		
+		return "Welcome to Spring Boot World";
+	}
+	
+}
